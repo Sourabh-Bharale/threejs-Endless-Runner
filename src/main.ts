7a4073e13d5c25e2d356078ad285fe5851121a1e
Sourabh-Bharale <sourabhbharale80@gmail.com>
2023-03-28 14:52:24 +0530

    assets added

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,37 @@
+import './style.css'
 
-import {
-  WebGLRenderer, PerspectiveCamera, Scene, BoxGeometry, MeshPhongMaterial, Mesh, DirectionalLight,
-} from 'three';
+import { WebGLRenderer, PerspectiveCamera, Scene, BoxGeometry, MeshPhongMaterial, Mesh, DirectionalLight } from 'three';
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 const renderer = new WebGLRenderer({
-  canvas: document.getElementById('app') as HTMLCanvasElement,
-  antialias: true,
-  precision: 'mediump',
+  canvas: document.getElementById('app') as HTMLCanvasElement
 });
+renderer.setSize(width, height);
 
 const mainCamera = new PerspectiveCamera(60, width / height, 0.1, 1000);
-function onWindowResize() {
-  mainCamera.aspect = window.innerWidth / window.innerHeight;
-  mainCamera.updateProjectionMatrix();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-window.addEventListener('resize', onWindowResize);
-
-renderer.setSize(width, height);
+const scene = new Scene();
 
 const geometry = new BoxGeometry();
 const material = new MeshPhongMaterial({ color: 0x0000ff });
 const cube = new Mesh(geometry, material);
-cube.position.z = -5;
-const scene = new Scene();
+cube.position.set(0, 0, -5);
 
 scene.add(cube);
+
+const rotateCube = () => {
+  cube.rotation.y -= 0.03;
+  cube.rotation.z -= 0.01;
+}
 const light = new DirectionalLight(0xFFFFFF, 1);
-light.position.z = 2;
+light.position.set(0, 0, 2);
 scene.add(light);
 
-renderer.render(scene, mainCamera);
+const render = () => {
+  rotateCube();
+  renderer.render(scene, mainCamera);
+  requestAnimationFrame(render);
+}
+render();
